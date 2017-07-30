cf74261bf9370294fb6d79dd09345bbe44c556d9
omer19-meet <omer19@meet.mit.edu>
2017-07-30 16:26:11 +0300

    Stopping here

---
+++ stringfun.py
@@ -0,0 +1,14 @@
+def string_test(s):
+    if s[0] == s[-1] and len(s)>2:
+        return(True)
+    else:
+        return(False)
+
+def add_x(s):
+    return('x'+ s+ 'x')
+
+    
+    
+    
+    
+    
