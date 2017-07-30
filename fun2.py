d63f7110ba13743d78aaa87e7c27aa975f28ef62
omer19-meet <omer19@meet.mit.edu>
2017-07-30 11:52:37 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -3,9 +3,17 @@ def draw_1d(nu):
     sing = str(input("what sing you want?"))
     print(sing*nu)
 
+def draw_3d(rows,s, border_s , fill_s):
+    print(s*border_s)
+    for rows in range(rows):
+         print(border_s + fill_s*(s-2) + border_s)
+    print(s*border_s)
 
+def fib 
+      
 
-for loop in range(0):
-    print("*"*5)
+         
+        
+        
     
     
